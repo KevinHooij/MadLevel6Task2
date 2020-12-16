ee40a9a99def7a1653922c54ad5a84ccdf4e13d8
KevinHooij <kevin.hooijschuur@hva.nl>
2020-12-16 16:18:55 +0100

    Sets up the adapter

---
+++ app/src/main/java/com/example/madlevel6task2/MovieAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.madlevel6task2
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.item_movie.view.*
+
+class MovieAdapter(private val movies: List<MovieItem>, private val onClick: (MovieItem) -> Unit) :
+    RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
+
+    private lateinit var context: Context
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        context = parent.context
+
+        return ViewHolder(
+            LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int = movies.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(movies[position])
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        init {
+            itemView.setOnClickListener { onClick(movies[adapterPosition]) }
+        }
+
+        fun bind(movieItem: MovieItem) {
+            // Glide.with(context).load(movieItem.getImageUrl()).into(itemView.ivColor)
+
+        }
+    }
+}
